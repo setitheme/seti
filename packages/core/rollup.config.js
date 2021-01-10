cc196a41499a125681cd8f5cf7b6e072e197ed14
Jesse Weed <code@jesseweed.io>
2021-01-10 01:45:08 -0800

    fix lint error

--- packages/core/rollup.config.js
+++ packages/core/rollup.config.js
@@ -1,30 +1,28 @@
 // Rollup Plugins
 import typescript from 'rollup-plugin-typescript2';
 import { terser } from 'rollup-plugin-terser';
-import scss from 'rollup-plugin-scss'
+import scss from 'rollup-plugin-scss';
 
 export default {
- input: 'src/seti.core.ts', // our source file
- output: [
-  {
-    file: 'build/seti.core.js',
-    format: 'es'
-  },
-  {
-    file: 'build/seti.core.cjs.js',
-    format: 'cjs'
-  }
- ],
- plugins: [
-  typescript({
-   typescript: require('typescript'),
-  }),
-  scss({
-    output: 'build/seti.css',
-    include: [
-      'styles/seti.scss'
-    ],
-  }),
-  terser() // minifies generated bundles
- ]
+  input: 'src/seti.core.ts', // our source file
+  output: [
+    {
+      file: 'build/seti.core.js',
+      format: 'es',
+    },
+    {
+      file: 'build/seti.core.cjs.js',
+      format: 'cjs',
+    },
+  ],
+  plugins: [
+    typescript({
+      typescript: require('typescript'),
+    }),
+    scss({
+      output: 'build/seti.css',
+      include: ['styles/seti.scss'],
+    }),
+    terser(), // minifies generated bundles
+  ],
 };
